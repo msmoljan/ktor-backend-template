dafe40e484543d3fcebec141ad1d20ddf4cd790e
msmoljan <matko.smoljan2@gmail.com>
2020-01-25 15:36:31 +0100

    Add domain and data modules

---
+++ main/data/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "smoljan.io"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation(project(":main:domain"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "ktor-backend-template"
 
+include("main:domain", "main:data")
